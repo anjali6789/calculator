2f3a8ba0978626e15008194f7150af74e879391f
anjali6789 <78044831+anjali6789@users.noreply.github.com>
2024-09-18 16:47:50 +0530

    Update CalculatorTest.java

--- src/test/java/ut/CalculatorTest.java
+++ src/test/java/ut/CalculatorTest.java
@@ -6,46 +6,44 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-
 import com.example.Calculator;
 
 public class CalculatorTest {
 
-	static Calculator cl;
+    static Calculator cl;
 
-	@BeforeClass
-	public static void setUp() {
-		cl = new Calculator();
-	}
+    @BeforeClass
+    public static void setUp() {
+        cl = new Calculator();
+    }
 
-	@AfterClass
-	public static void tearDown() {
-		System.gc();
-	}
+    @AfterClass
+    public static void tearDown() {
+        System.gc();
+    }
 
-	@Test
-	public void testAdd() {
+    @Test
+    public void testAdd() {
+        assertEquals(3, cl.doAdd(1, 2));
+    }
 
-		assertTrue(cl.doAdd(1, 2) == 3);
-	}
+    @Test
+    public void testSub1() {
+        assertEquals(0, cl.doSub(1, 1));
+    }
 
-	@Test
-	public void testSub1() {
-		assertTrue(cl.doSub(1, 1) == 0);
-	}
+    @Test
+    public void testMul1() {
+        assertEquals(18, cl.doMul(9, 2));
+    }
+    
+    @Test
+    public void testSub2() {
+        assertEquals(0.0, cl.doSub(1.0, 1.0), 0.0);
+    }
 
-	@Test
-	public void testMul1() {
-		assertTrue(cl.doMul(9, 2) == 18);
-	}
-	
-	@Test
-	public void testSub2() {
-		assertEquals(0.0, cl.doSub(1.0, 1.0), 0.0);
-	}
-	@Test
-	public void testDiv() {
-		assertTrue(cl.doDiv(8, 2) == 4);
-	}
-
+    @Test
+    public void testDiv() {
+        assertEquals(4, cl.doDiv(8, 2));
+    }
 }
